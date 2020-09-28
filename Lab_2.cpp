f4bb2e4843955e3118c3404ca138951062fa2566
h8-me <71770413+h8-me@users.noreply.github.com>
2020-09-28 10:49:57 +0300

    Add files via upload

---
+++ Lab_2.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+using namespace std;
+
+
+class MyClass {
+public:
+	static int num;
+	int id;
+	MyClass() {
+		id = ++num;
+	};
+
+	void printNumber() {
+		cout << "Мой порядковый номер: " << id << endl;
+	};
+};
+int MyClass::num = 0;
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+
+	MyClass* i1 = new MyClass();
+	MyClass* i2 = new MyClass();
+	MyClass* i3 = new MyClass();
+	MyClass* i4 = new MyClass();
+
+	i1->printNumber();
+	i2->printNumber();
+	i3->printNumber();
+	i4->printNumber();
+	system("pause");
+	return 0;
+}
